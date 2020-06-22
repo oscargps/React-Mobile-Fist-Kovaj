93eb9d49e73f2d79955f6d5acaad64b513e099a3
Oscar <oscar9557@hotmail.com>
2020-06-22 16:16:07 -0500

    Estado de salud del personal

--- src/components/EstadodeSalud.jsx
+++ src/components/EstadodeSalud.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import './styles/EstadodeSalud.css'
+import healthStatus from '../static/air-pollution.svg'
 const EstadodeSalud = () =>{
     return(
         <div className="EstadodeSalud">
-            aqui va el estado de salud
+            <p className="EstadodeSalud__title">Estado de Salud del Personal</p>
+            <img src={healthStatus} alt="Estado de salud" className="EstadodeSalud__Imagen"/>
+            <p className="EstadodeSalud__Porcentaje">
+            98%
+            </p>
+            <p className="EstadodeSalud__more">Ver mas...</p>
+
         </div>
     )
 }
